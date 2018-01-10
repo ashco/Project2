840cce442855d31d71a75eb8282311aebb0ffd64
ashco <ashchristie@gmail.com>
2018-01-10 10:50:56 -0800

    public folder works again

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,

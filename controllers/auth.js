4302a7211f3cf66951a6f3f45d4114587531adde
ashco <ashchristie@gmail.com>
2018-01-11 13:13:56 -0800

    housekeeping

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,7 @@ router.get('/login', function(req, res){
 });
 
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/portfolio',
+  successRedirect: '/watchlist',
   successFlash: 'Login Successful',
   failureRedirect: '/auth/login',
   failureFlash: 'Invalid Credentials'

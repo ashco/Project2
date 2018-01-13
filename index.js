56c111c4fb259aa0296c83143a41a61ef8511c53
ashco <ashchristie@gmail.com>
2018-01-12 20:21:52 -0800

    adding styling, working on portfolio modal

--- index.js
+++ index.js
@@ -1,76 +1,74 @@
 //Declare Modules
-require('dotenv').config();
-var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
-var ejsLayouts = require('express-ejs-layouts');
-var flash = require('connect-flash');
-var isLoggedIn = require('./middleware/isLoggedIn.js');
-var pagePop = require('./middleware/pagePopulator.js');
-var passport = require('./config/ppConfig.js');
-var session = require('express-session');
+require("dotenv").config();
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+var ejsLayouts = require("express-ejs-layouts");
+var flash = require("connect-flash");
+var isLoggedIn = require("./middleware/isLoggedIn.js");
+var pagePop = require("./middleware/pagePopulator.js");
+var passport = require("./config/ppConfig.js");
+var session = require("express-session");
 var app = express();
 
 //API URLs
-var tickerURL = 'https://api.coinmarketcap.com/v1/ticker/?limit=25';
-var tmcURL = 'https://api.coinmarketcap.com/v1/global/'
+var tickerURL = "https://api.coinmarketcap.com/v1/ticker/?limit=25";
+var tmcURL = "https://api.coinmarketcap.com/v1/global/";
 
 //API data
 var tickerData;
 var tmcData;
 
-
-function getData(){
-  request(tickerURL, function(error, response, body){
+function getData() {
+  request(tickerURL, function(error, response, body) {
     tickerData = JSON.parse(body);
-    request(tmcURL, function(error, response, body){
+    request(tmcURL, function(error, response, body) {
       tmcData = JSON.parse(body);
     });
   });
-  console.log('API data got!');
-};  
+  console.log("API data got!");
+}
 
-getData()
+getData();
 
 //MIDDLEWARE
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(express.static(__dirname + '/public'));
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(express.static(__dirname + "/public"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.alerts = req.flash();
   next();
 });
 
-
 //Root directory
-app.get('/', function(req, res){
-  res.render('marketcap.ejs', {
-    tickerData : tickerData,
+app.get("/", function(req, res) {
+  res.render("marketcap.ejs", {
+    tickerData: tickerData,
     tmcData: tmcData
   });
 });
 
-app.get('/marketcap', function(req, res){
-  res.redirect('/');
+app.get("/marketcap", function(req, res) {
+  res.redirect("/");
 });
 
-
 //Declaring Routes
-app.use('/auth', require('./controllers/auth.js'));
-app.use('/watchlist', require('./controllers/watchlist.js'));
-app.use('/portfolio', require('./controllers/portfolio.js'));
+app.use("/auth", require("./controllers/auth.js"));
+app.use("/watchlist", require("./controllers/watchlist.js"));
+app.use("/portfolio", require("./controllers/portfolio.js"));
 // app.use('/wallets', require('./controllers/wallets.js'));
-
 
 var server = app.listen(process.env.PORT || 3000);
 
